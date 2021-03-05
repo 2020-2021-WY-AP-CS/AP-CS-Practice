ccc071ee3f210705a8845a4cfb3afa562f4d3802
Mauer-Oats <maueroats@users.noreply.github.com>
2021-03-05 12:26:38 -0600

    Create AP2006.java

---
+++ AP2006.java
@@ -0,0 +1,34 @@
+/* 
+AP CS A 2006 Questions 1 and 2.
+Do not search the internet.
+Just sit down and write your best response.
+It does not need to compile (in fact it will not unless you add a ton of unnecessary code).
+*/
+class Main {
+  public class Appointment {
+      // Question 1(a): write conflictsWith
+    public boolean conflictsWith(Appointment other) {
+      return false;
+    }
+  }
+  public class DailySchedule {
+      // Question 1(b): write clearConflicts
+    public void clearConflicts(Appointment appt) {
+    }
+    // Question 1(c): Write addAppt
+    public boolean addAppt(Appointment appt, boolean emergency) {
+      return false;
+    }
+  }
+
+  public class TaxableItem implements Item {
+    // Question 2(a)
+    public double purchasePrice() {
+      return 0;
+    }
+    // Question 2(b): create the entire Vehicle class as described
+
+
+    // end of Vehicle class
+  }
+}
